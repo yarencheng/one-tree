39992351f5286340cae49bae1a5df3d03fa88319
Aren Chen <yarencheng@gmail.com>
2019-07-19 03:19:53 +0800

    test

--- go-src/cmd/hello-world/main.go
+++ go-src/cmd/hello-world/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/sha256"
+	"fmt"
+	"yarencheng/one-tree/go-src/block"
+	"github.com/ethereum/go-ethereum/crypto"
+)
 
 func main() {
 	fmt.Println("Hello blockchain")
+
+	privateKey, _ := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+
+	b := block.SignFirstBlock(privateKey)
+
+	fmt.Println("b = %#v", b)
+
+	pb := fmt.Sprintf("%#v", b.Payload)
+	hash := sha256.Sum256([]byte(pb))
+
+	valid := crypto.VerifySignature(crypto.FromECDSAPub(&privateKey.PublicKey), hash[:], b.Signature)
+
+	fmt.Println("valid = %#v", valid)
 }
--- go-src/go.mod
+++ go-src/go.mod
@@ -1,3 +1,11 @@
 module yarencheng/one-tree/go-src
 
 go 1.12
+
+require (
+	github.com/ethereum/go-ethereum v1.9.0
+	github.com/kr/pretty v0.1.0 // indirect
+	github.com/satori/go.uuid v1.2.0
+	golang.org/x/crypto v0.0.0-20190701094942-4def268fd1a4 // indirect
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
+)
